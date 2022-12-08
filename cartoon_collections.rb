abcef83c70e95ad80a03bac2148aab5cc371af8b
Steven McVey <smcvey1210@gmail.com>
2022-12-08 12:44:51 -0800

    commit tree 93faf3a5a5b94956c7a7c29519999353dcd68fa0

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each.with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |command|
+    "#{command.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  truth = false
+  array.each {|call| call.length > 4 ? truth = true : truth = truth}
+
+  truth
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese = false
+  final = nil
+
+  array.each do |item|
+    # binding.pry
+    cheese_types.include?(item) ? cheese = true : cheese = cheese
+    final = item if cheese == true
+    break if cheese == true
+  end
+    final
 end
+
+puts find_the_cheese(["crackers", "gouda", "thyme"])
